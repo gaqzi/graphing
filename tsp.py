252e97dd0f0f6488d8f7c48348a3e26b21663e35
Björn Andersson <ba@sanitarium.se>
2021-11-27 12:15:03 +0800

    Rename for clarity

--- tsp.py
+++ tsp.py
@@ -9,22 +9,21 @@ def travel(from_, to, graph_data):
         where[start].append((end, type_))
         where[end].append((start, type_))
 
-    options = _find_all(to, from_, where)
-
-    if not options:
+    paths = _find_all_paths(to, from_, where)
+    if not paths:
         raise Exception('did not find any paths :(')
 
     path_by_flights = defaultdict(list)
     min_flights = math.inf
-    for opt in options:
-        for num_flights, path in _all_paths_with_flights(opt):
-            path_by_flights[num_flights].append(path)
+    for path in paths:
+        for num_flights, p in _all_paths_with_num_flights(path):
+            path_by_flights[num_flights].append(p)
             min_flights = min(num_flights, min_flights)
 
     return list(min(path_by_flights[min_flights], key=len))
 
 
-def _find_all(to, from_, where, visited=None):
+def _find_all_paths(to, from_, where, visited=None):
     visited = visited or [from_]
     path = [from_]
     connections = where[from_]
@@ -38,7 +37,7 @@ def _find_all(to, from_, where, visited=None):
             options.append(path + [conn])
             continue  # if it's where we're going to then we've found a full path
 
-        ps = _find_all(to, conn[0], where, visited + [conn[0]])
+        ps = _find_all_paths(to, conn[0], where, visited + [conn[0]])
         if ps:
             for i, _ in enumerate(ps):
                 ps[i][0] = conn
@@ -48,7 +47,7 @@ def _find_all(to, from_, where, visited=None):
     return options
 
 
-def _all_paths_with_flights(options):
+def _all_paths_with_num_flights(options):
     path = []
     num_flights = 0
     has_returned_subpaths = False
@@ -59,7 +58,7 @@ def _all_paths_with_flights(options):
                 num_flights += 1
             path.append(node[0])
         elif isinstance(node, list):
-            for num, pp in _all_paths_with_flights(node):
+            for num, pp in _all_paths_with_num_flights(node):
                 yield num_flights + num, path + pp
                 has_returned_subpaths = True
         else:
